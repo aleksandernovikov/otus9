820be90ea96b2f25b38dd1bdf517fe71bf8c8136
sanya <79fz270704@gmail.com>
2020-03-25 09:26:25 +0300

    to babel 7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
-let register_form = document.getElementById('register_form')
+// let register_form = document.getElementById('register_form')
 
 //https://stackoverflow.com/questions/15467024/hack-for-jquery-to-serialize-values-of-input-and-select-elements-that-are-no
-register_form.addEventListener('submit', function (e) {
-    e.preventDefault()
-    console.log(e)
-})
+// register_form.addEventListener('submit', function (e) {
+//     e.preventDefault()
+//     console.log(e)
+// })
+let v;
